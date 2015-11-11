dae00a5e6723324eeb6143f6f162f03c1614c309
tcontois4 <alassall@umass.edu>
2015-11-11 00:56:20 -0500

    obama speeches

--- markov_runner.py
+++ markov_runner.py
@@ -2,6 +2,6 @@ from __future__ import division
 import markov_text_gen
 from collections import defaultdict
 
-file_=open('input.txt')
+file_=open('obama_speeches.txt')
 markov=markov_text_gen.Markov(file_)
 print markov.generate_markov_text()
