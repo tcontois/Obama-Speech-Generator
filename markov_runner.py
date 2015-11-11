b3acdf91274d417dbe644d57413533b614a5040f
tcontois4 <alassall@umass.edu>
2015-11-11 13:30:42 -0500

    modified markov

--- markov_runner.py
+++ markov_runner.py
@@ -1,7 +1,12 @@
 from __future__ import division
 import markov_text_gen
+import modified_markov
 from collections import defaultdict
 
 file_=open('obama_speeches.txt')
 markov=markov_text_gen.Markov(file_)
+print "Markov chain looking at previous 2 words:"
 print markov.generate_markov_text()
+print "\nNow looking at previous 3 words \n"
+markov_modified=modified_markov.Markov(file_, chain_size=3)
+print markov_modified.generate_markov_text()
