0ff3cf1e52c2a5c976cbf30b216cc8349bfdd8cc
tcontois <tcontois@umass.edu>
2015-11-11 23:43:06 -0500

    example oujtput

--- markov_runner.py
+++ markov_runner.py
@@ -11,9 +11,11 @@ file_=open('obama_speeches.txt')
 # markov=markov_text_gen.Markov(file_)
 #print "Markov chain looking at previous 2 words:"
 # print markov.generate_markov_text()
-print "\nNow looking at previous 3 words \n"
-markov_modified=modified_markov.Markov(file_, chain_size=3)
-print markov_modified.generate_markov_text()
+for i in range(2,5):
+	markov_modified=modified_markov.Markov(file_, chain_size=(i))
+	print "Examples with chain length %s" %i
+	for j in range(10):
+		print "Example #%s: \n" %(j+1) + markov_modified.generate_markov_text()
 # tims_markov=tims_text_gen.Markov(file_)
 # print "Markov chain looking at previous 2 words:"
 # print ''
