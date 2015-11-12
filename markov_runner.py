9c05d6202b15227055466b6dfcc5c145e733ba50
tcontois <tcontois@umass.edu>
2015-11-11 22:22:34 -0500

    tims text gen

--- markov_runner.py
+++ markov_runner.py
@@ -1,12 +1,17 @@
 from __future__ import division
 import markov_text_gen
 import modified_markov
+import tims_text_gen
 from collections import defaultdict
 
 file_=open('obama_speeches.txt')
-markov=markov_text_gen.Markov(file_)
-print "Markov chain looking at previous 2 words:"
-print markov.generate_markov_text()
+# markov=markov_text_gen.Markov(file_)
+# print "Markov chain looking at previous 2 words:"
+# print markov.generate_markov_text()
 # print "\nNow looking at previous 3 words \n"
-# markov_modified=modified_markov.Markov(file_, chain_size=3)
+# markov_modified=modified_markov.Markov(file_, chain_size=2)
 # print markov_modified.generate_markov_text()
+tims_markov=tims_text_gen.Markov(file_)
+print "Markov chain looking at previous 2 words:"
+print ''
+print tims_markov.generate_markov_text()
