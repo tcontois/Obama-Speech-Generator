556450a22f76406dafc2e0337c7fd99c62f7ccd6
tcontois4 <alassall@umass.edu>
2015-12-17 11:44:43 -0500

    pos_model change

--- pos_model.py
+++ pos_model.py
@@ -1,11 +1,60 @@
-'''
-PLEASE TRY THIS JOSH - maybe txblob will help!!! :)
-try making model that fills in an part of speech sequence with Obama terms
-steps:
-1. find common pos sequences either from tagging Obama sequences or online
-2. for each tag, store Obama words with that pos tag
-3. when generating sentence, go through pos tag sequence and at each step pick
-a word with that pos tag (ideally weighed more heavily based on topic)
+from collections import defaultdict
+from textblob import TextBlob
+import sequence
+import random
 
-we can compare this model to our markov n-gram model
-'''
+def getwords():
+	filew = open('datasets/dictionaryforalex.txt','w')
+	file = open('datasets/short_fp.txt')
+	text = file.read()
+	t = TextBlob(text.decode('ascii', 'ignore'))
+	#t = TextBlob(text)
+	wordlist = t.tags
+	taglist = {}
+	#print wordlist
+	for k,v in wordlist:
+		filew.write(k+" "+v+'\n')
+		if v in taglist:
+			taglist[v].append(k)
+		else:
+			taglist[v] = [k]
+	return taglist
+
+
+
+def writeseq():
+	filew = open('datasets/sequence.txt','w')
+	file = open('datasets/obama_speeches.txt')
+	text = file.read()
+	t = TextBlob(text.decode('ascii','ignore'))
+	#t = TextBlob("Hello. This is a sample text. I fuckin hate alex lSSALLE.! yes !")
+	wordlist = t.tags
+	taglist = {}
+	for k,v in wordlist:
+		filew.write(v+" ")
+
+
+if __name__ == '__main__':
+	writeseq()
+	open_file=open('./datasets/sequence.txt')
+	pos_dictionary=getwords()
+
+	sequence_markov=sequence.Markov(open_file)
+	pos_sequence= sequence_markov.generate_markov_text().split()
+	sentence=''
+	first=True
+	for x in range(0,5):
+		for tags in pos_sequence:
+			if (first):
+				sentence=pos_dictionary[tags][random.randint(0,len(pos_dictionary[tags])-1)]
+				while not sentence[0].isupper():
+					sentence=pos_dictionary[tags][random.randint(0,len(pos_dictionary[tags])-1)]
+				first=False
+			else:
+				sentence=sentence+' '+ pos_dictionary[tags][random.randint(0,len(pos_dictionary[tags])-1)]
+		print sentence
+		print
+		sentence=''
+
+	getwords()
+
